c7703dd11d7b0d897aa501f90496d4053f2f5deb
MlxySF <85721532+MlxySF@users.noreply.github.com>
2025-12-21 07:46:28 +0800

    Update body.php

--- pages/register-parts/body.php
+++ pages/register-parts/body.php
@@ -1,7 +1,7 @@
 <?php
 // Generate CSRF token if not exists
 if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));\n}
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));}
 ?>
 
 
