5976f082f2db78c9b5294088ed4c45f87af079dd
MlxySF <85721532+MlxySF@users.noreply.github.com>
2025-12-22 11:29:28 +0800

    Fix jQuery CDN error - Change from code.jquery.com to cdnjs.cloudflare.com

--- admin.php
+++ admin.php
@@ -1062,7 +1062,8 @@ $page = $_GET['page'] ?? 'login';
     </div>
 <?php endif; ?>
 
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<!-- FIXED: Changed jQuery CDN from code.jquery.com to cdnjs.cloudflare.com -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
